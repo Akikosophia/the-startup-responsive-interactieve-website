c4c0f7af7e406969b8bfa3c186e227cb7df65227
Akikosophia <144008863+Akikosophia@users.noreply.github.com>
2024-01-26 08:58:47 +0100

    JS

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 
+const slider = document.querySelector ('.slider');
+
+const leftArrow = document.querySelector ('.left');
+const rightArrow = document.querySelector ('.right');
+
+let sectionIndex = 0;
+
+rightArrow.addEventListener ('click' , function() {
+  sectionIndex = (sectionIndex < 3 ) ? sectionIndex + 1 : 3;
+  slider.style.transform ='translate(' + (sectionIndex) * -25 + '%)';
+});
